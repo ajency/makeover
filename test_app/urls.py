c88b117119ba971aeca052177c5ca7b3131465b6
Robiul Hoque <robiul@ajency.in>
2016-08-24 19:34:15 +0530

    changed url pattern

--- test_app/urls.py
+++ test_app/urls.py
@@ -2,10 +2,18 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from test_app import views
 
-urlpatterns = [
+# urlpatterns = [
+#     url(r'^auth/', views.AuthView.as_view(), name='auth-view'),
+#     url(r'^requestpages/', views.RequestPagesView.as_view(), name='request-pages-view'),
+#     url(r'^checkrequestgetpages/', views.CheckRequestPagesView.as_view(), name='check-request-pages-view'),
+#     url(r'^getpages/', views.GetPagesView.as_view(), name='check-request-pages-view'),
+#     url(r'^', views.TestView.as_view(), name='test-view'),
+# ]
+
+urlpatterns = patterns('',
     url(r'^auth/', views.AuthView.as_view(), name='auth-view'),
     url(r'^requestpages/', views.RequestPagesView.as_view(), name='request-pages-view'),
     url(r'^checkrequestgetpages/', views.CheckRequestPagesView.as_view(), name='check-request-pages-view'),
     url(r'^getpages/', views.GetPagesView.as_view(), name='check-request-pages-view'),
     url(r'^', views.TestView.as_view(), name='test-view'),
-]
+)
