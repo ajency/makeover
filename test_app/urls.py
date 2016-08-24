2960a17aeb586715fe2ed9a210c0d0f02d56a09b
Robiul Hoque <robiul@ajency.in>
2016-08-24 19:37:52 +0530

    removed pattern

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,19 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from test_app import views
 
-# urlpatterns = [
-#     url(r'^auth/', views.AuthView.as_view(), name='auth-view'),
-#     url(r'^requestpages/', views.RequestPagesView.as_view(), name='request-pages-view'),
-#     url(r'^checkrequestgetpages/', views.CheckRequestPagesView.as_view(), name='check-request-pages-view'),
-#     url(r'^getpages/', views.GetPagesView.as_view(), name='check-request-pages-view'),
-#     url(r'^', views.TestView.as_view(), name='test-view'),
-# ]
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^auth/', views.AuthView.as_view(), name='auth-view'),
     url(r'^requestpages/', views.RequestPagesView.as_view(), name='request-pages-view'),
     url(r'^checkrequestgetpages/', views.CheckRequestPagesView.as_view(), name='check-request-pages-view'),
     url(r'^getpages/', views.GetPagesView.as_view(), name='check-request-pages-view'),
     url(r'^', views.TestView.as_view(), name='test-view'),
-)
+]
