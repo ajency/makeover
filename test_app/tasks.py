3a72cd6be9e526ff9a04d297086fec55644b8db2
vardan10 <vardan@ajency.in>
2016-08-25 11:04:53 +0530

    Add files via upload

--- test_app/tasks.py
+++ test_app/tasks.py
@@ -7,13 +7,16 @@ logger = get_task_logger(__name__)
 
 from rest_framework.response import Response
 from test_app.models import first
+from test_app.models import second
 
-@task(name="scraper")
-def scraper(url,domain):
+
+
+@task(name="crawler")
+def crawler(url,domain):
     
     """sends an email when feedback form is filled successfully"""
-    print "====================" + scraper.request.id + "======================="
-    print "I am inside scraper"
+    print "====================" + crawler.request.id + "======================="
+    print "I am inside Crawler"
     import scraper_quick
     xyz = scraper_quick.main(url,domain)
     print "I finished running"
@@ -26,7 +29,30 @@ def scraper(url,domain):
     print data
 
     data = str(data)
-    reqId = str(scraper.request.id.encode('ascii','ignore'))
+    reqId = str(crawler.request.id.encode('ascii','ignore'))
 
     p = first(reqId=reqId , domainName=domain , data=data)
+    p.save()
+
+
+
+
+
+@task(name="scraper")
+def scraper(data,domain):
+    
+    """sends an email when feedback form is filled successfully"""
+    print "====================" + scraper.request.id + "======================="
+    print "I am inside scraper"
+
+    urls = data.split(',')
+
+    import scraper_quick2
+    xyz = scraper_quick2.main(urls,domain)
+    print "I finished running"
+
+    reqId = str(scraper.request.id.encode('ascii','ignore'))
+    data = str(xyz[0]).replace('\"', "'").encode('utf-8')
+    header = str(xyz[1]).replace('\"', "'").encode('utf-8')
+    p = second(reqId=reqId , domainName=domain , data=data, header=header)
     p.save()
