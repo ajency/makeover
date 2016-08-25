6ee6b6bc99253a126f555a94f5bd23b05c2c60a1
Robiul Hoque <robiul@ajency.in>
2016-08-25 13:41:13 +0530

    wsgi updated

--- test_project/settings.py
+++ test_project/settings.py
@@ -129,7 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = '/var/www/python/ENV/makeover/test_project/static/'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
